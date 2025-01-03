a428fa39ba6d6c7819fffac79798ae018ddb6cb9
OST <cpython.projects@gmail.com>
2025-01-03 21:11:08 +0300

    django 9 edit static root

--- main/context_processors.py
+++ main/context_processors.py
@@ -4,15 +4,29 @@ from .models import ContactInfo
 def contact_info(request):
     data = ContactInfo.objects.first()
 
-    return {
-        'city': data.city_address,
-        'street': data.street_address,
-        'phone_number': data.phone_number,
-        'email': data.email,
-        'opening_hours': data.opening_hours,
+    if data:
+        return {
+            'city': data.city_address ,
+            'street': data.street_address,
+            'phone_number': data.phone_number,
+            'email': data.email,
+            'opening_hours': data.opening_hours,
 
-        'facebook': data.facebook_link,
-        'x': data.x_link,
-        'instagram': data.instagram_link,
-        'linkedin': data.linkedin_link,
+            'facebook': data.facebook_link,
+            'x': data.x_link,
+            'instagram': data.instagram_link,
+            'linkedin': data.linkedin_link,
+        }
+
+    return {
+        'city': '',
+        'street': '',
+        'phone_number': '',
+        'email': '',
+        'opening_hours': '',
+
+        'facebook': '',
+        'x': '',
+        'instagram': '',
+        'linkedin': '',
     }
